23feabeea5328968085049e2a4ae51de64d1d627
Stylianos Kyprianou <43478713+stylianosk0@users.noreply.github.com>
2025-02-22 16:17:52 +0200

    Update search.php

--- SearchCustomer/search.php
+++ SearchCustomer/search.php
@@ -1,16 +1,6 @@
 <?php
-// Database connection
-$servername = "localhost";
-$username = "root";  // Your database username
-$password = "";      // Your database password
-$dbname = "MobileGarage"; // Your database name
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check for connection errors
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// Include the database connection file
+require_once 'db_connection.php';
 
 // Check if the search query is set
 if (isset($_GET['query'])) {
