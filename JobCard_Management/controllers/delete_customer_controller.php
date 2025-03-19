b524f4847dfdff67c153b25eb3d00e25a5f2f487
antonis10 <andreou261@gmail.com>
2025-03-19 10:47:56 +0200

    Update delete_customer_controller.php

--- JobCard_Management/controllers/delete_customer_controller.php
+++ JobCard_Management/controllers/delete_customer_controller.php
@@ -1,7 +1,25 @@
 <?php
-	session_start();
-	require_once "../models/customer_model.php";
-	$customerMang = new customerManagement();
-	
-	$customerMang->Delete();
-?>
+session_start();
+require_once "../models/job_card_model.php";
+
+try {
+    if (!isset($_POST['id'])) {
+        throw new Exception("Job card ID not provided");
+    }
+
+    if (JobCard::delete($_POST['id'])) {
+        $_SESSION['message'] = "Job card deleted successfully!";
+        $_SESSION['message_type'] = "success";
+    } else {
+        throw new Exception("Failed to delete job card");
+    }
+
+    header("Location: ../views/job_cards_main.php");
+    exit;
+} catch (Exception $e) {
+    $_SESSION['message'] = "Error: " . $e->getMessage();
+    $_SESSION['message_type'] = "error";
+    header("Location: ../views/job_cards_main.php");
+    exit;
+}
+?> 
