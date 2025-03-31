3bb992d5fe651ee506cda66db00d0ec4d8fe2651
antonis10 <andreou261@gmail.com>
2025-03-31 17:46:45 +0300

    Update PrintSelectedJobs.php

--- JobCard_Management/views/print/PrintSelectedJobs.php
+++ JobCard_Management/views/print/PrintSelectedJobs.php
@@ -12,13 +12,11 @@ if (empty($selectedIds)) {
 $placeholders = str_repeat('?,', count($selectedIds) - 1) . '?';
 
 // SQL query to fetch selected job cards
-$sql = "SELECT j.JobID, j.Location, j.DateCall, j.JobDesc, j.DateStart, j.DateFinish, j.DriveCosts,
+$sql = "SELECT j.JobID, j.Location, j.DateCall, j.JobDesc, j.DateStart, j.DateFinish,
         CONCAT(c.FirstName, ' ', c.LastName) as CustomerName, 
         car.LicenseNr, car.Brand, car.Model, 
         pn.Nr as PhoneNumber,
-        a.Address,
-        (SELECT SUM(PricePerPiece * PiecesSold) FROM JobCardParts WHERE JobID = j.JobID) as PartsTotal,
-        j.DriveCosts as AdditionalCost
+        a.Address
         FROM JobCards j 
         LEFT JOIN JobCar jc ON j.JobID = jc.JobID
         LEFT JOIN Cars car ON jc.LicenseNr = car.LicenseNr
@@ -122,9 +120,6 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <th>Phone</th>
                 <th>Job Start/End date</th>
                 <th>Status</th>
-                <th>Parts Total</th>
-                <th>Additional Cost</th>
-                <th>Total Cost</th>
             </tr>
         </thead>
         <tbody>
@@ -158,24 +153,6 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         } else {
                             echo '<span class="status-open">OPEN</span>';
                         }
-                        ?>
-                    </td>
-                    <td class="cost">
-                        <?php 
-                        $partsTotal = floatval($row['PartsTotal'] ?: 0);
-                        echo '€' . number_format($partsTotal, 2);
-                        ?>
-                    </td>
-                    <td class="cost">
-                        <?php 
-                        $additionalCost = floatval($row['AdditionalCost'] ?: 0);
-                        echo '€' . number_format($additionalCost, 2);
-                        ?>
-                    </td>
-                    <td class="cost">
-                        <?php 
-                        $totalCost = $partsTotal + $additionalCost;
-                        echo '€' . number_format($totalCost, 2);
                         ?>
                     </td>
                 </tr>
